01df305ed065a356c27d888c038f99ee07e49273
sincherer <ersincher@gmail.com>
2025-04-02 14:41:09 +0800

    chore: update workflow

--- vite.config.live.ts
+++ vite.config.live.ts
@@ -4,8 +4,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wui/',
   build: {
     outDir: "live",
     emptyOutDir: true,
+    sourcemap: false
   },
 });
